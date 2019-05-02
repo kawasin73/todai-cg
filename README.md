8d8b82f476c72b3f8ecbb4567f0beb937f6de8bf
Kawamura Shintaro <kawasin73@gmail.com>
2019-05-02 21:40:51 +0900

    Add legacygl.js

--- README.md
+++ README.md
@@ -13,6 +13,17 @@ http://research.nii.ac.jp/~takayama/teaching/utokyo-iscg-2019/
 課題PDF : http://research.nii.ac.jp/~takayama/teaching/utokyo-iscg-2019/assignments/iscg-2019-assignment-1-spline.pdf
 サンプル : https://utokyo-iscg-2019-assigment-spline.glitch.me/
 
+## 開発
+
+```bash
+$ brew install git caddy
+$ git clone https://github.com/kawasin73/todai-cg.git ./repo_name
+$ cd ./repo_name
+$ ./import.sh
+$ caddy -host localhost -root ./public -port 3000
+$ open http://localhost:3000
+```
+
 ## LICENSE
 
 MIT
---
+++ import.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env sh
+
+set -eu
+
+REPO_URL="https://github.com/kawasin73/legacygl.js.git"
+REPO_DIR="./legacygl.js"
+DEST_DIR="./public/lib"
+DEST="${DEST_DIR}/legacygl.js"
+
+#
+# download legacygl.js
+#
+if [ ! -d ${REPO_DIR} ]; then
+    git clone ${REPO_URL} ${REPO_DIR}
+fi
+
+#
+# combine legacygl.js into 1 file
+#
+
+# create library dir
+mkdir -p ${DEST_DIR}
+
+# remove library file if exists
+if [ -e ${DEST} ]; then
+    echo "remove dest file ${DEST}"
+    rm ${DEST}
+fi
+
+echo "combine each file to legacygl.js"
+
+# add file header
+echo "// ====================================================" >> ${DEST}
+echo "// legacygl.js"                                          >> ${DEST}
+echo "// Copy From https://bitbucket.org/kenshi84/legacygl.js" >> ${DEST}
+echo "// ====================================================" >> ${DEST}
+
+for pathname in ./legacygl.js/*.js; do
+
+    echo "combine ${pathname}"
+
+    # append header to file
+    echo ""                                                        >> ${DEST}
+    echo "// ====================================================" >> ${DEST}
+    echo "// filename: ${pathname}"                                >> ${DEST}
+    echo "// ====================================================" >> ${DEST}
+
+    # append source code
+    cat ${pathname} >> ${DEST}
+done
+
+echo "created dest file ${DEST}"
